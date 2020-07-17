78a4a4f611ecdc3d79a80ea4286be18e9e3d0911
timba64 <timba64@mail.ru>
2020-07-17 19:05:51 +0300

    try add image in easy admin

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -11,6 +11,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,38 +20,40 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CommentCrudController extends AbstractCrudController
 {
-    public static function getEntityFqcn(): string
-    {
-        return Comment::class;
+	public static function getEntityFqcn(): string
+	{
+			return Comment::class;
 	}
 		
-    public function configureCrud(Crud $crud): Crud
-    {
-        return $crud
-            // the labels used to refer to this entity in titles, buttons, etc.
-            ->setEntityLabelInSingular('Comment')
-            ->setEntityLabelInPlural('Comments')
+	public function configureCrud(Crud $crud): Crud
+	{
+			return $crud
+					// the labels used to refer to this entity in titles, buttons, etc.
+					->setEntityLabelInSingular('Comment')
+					->setEntityLabelInPlural('Comments')
 
-            // the Symfony Security permission needed to manage the entity
-            // (none by default, so you can manage all instances of the entity)
-            ->setEntityPermission('ROLE_ADMIN')
-            // the visible title at the top of the page and the content of the <title> element
-            // it can include these placeholders: %entity_id%, %entity_label_singular%, %entity_label_plural%
-            ->setPageTitle('index', '%entity_label_plural% listing')
-        ;
-    }
+					// the Symfony Security permission needed to manage the entity
+					// (none by default, so you can manage all instances of the entity)
+					->setEntityPermission('ROLE_ADMIN')
+					// the visible title at the top of the page and the content of the <title> element
+					// it can include these placeholders: %entity_id%, %entity_label_singular%, %entity_label_plural%
+					->setPageTitle('index', '%entity_label_plural% listing')
+			;
+	}
     
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            //IdField::new('id'),
-            AssociationField::new('conference'),
-            TextField::new('author'),
-            TextEditorField::new('text'),
-            EmailField::new('email'),
-            DateTimeField::new('createdAt'),
-            ImageField::new('photo_filename')
-        ];
+	public function configureFields(string $pageName): iterable
+	{
+		return [
+				//IdField::new('id'),
+				AssociationField::new('conference'),
+				TextField::new('author'),
+				TextEditorField::new('text'),
+				EmailField::new('email'),
+				DateTimeField::new('createdAt'),
+				ImageField::new('photoFile')->onlyOnForms()->setTextAlign('left'),
+				ImageField::new('photoName')->setBasePath('upload/images')->hideOnForm(),
+				IntegerField::new('imageSize')
+		];
 	}
 		
 }
--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -4,9 +4,12 @@ namespace App\Entity;
 
 use App\Repository\CommentRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
- * @ORM\Entity(repositoryClass=CommentRepository::class)
+ * @ORM\Entity
+ * @Vich\Uploadable
  */
 class Comment
 {
@@ -37,6 +40,11 @@ class Comment
      */
     private $createdAt;
 
+		/**
+     * @ORM\Column(type="datetime", name="updated_at")
+     */
+    private $updatedAt;
+
     /**
      * @ORM\ManyToOne(targetEntity=Conference::class, inversedBy="comments")
      * @ORM\JoinColumn(nullable=false)
@@ -44,13 +52,33 @@ class Comment
     private $conference;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(type="string", length=255)
+		 * @var string|null
      */
-		private $photoFilename;
+		private $photoName;
+
+		/**
+		 * @Vich\UploadableField(mapping="comment_images", fileNameProperty="photoName", size="imageSize")
+		 * @var File|null
+		 */
+		private $photoFile;
+
+		/**
+     * @ORM\Column(type="integer")
+     *
+     * @var int|null
+     */
+    private $imageSize;
 		
 		public function __toString(): string
 		{
 			return (string) $this->getEmail();
+		}
+		
+		public function __construct()
+		{
+			$this->createdAt = new \DateTime();
+			$this->updatedAt = new \DateTime();
 		}
 
     public function getId(): ?int
@@ -116,16 +144,61 @@ class Comment
         $this->conference = $conference;
 
         return $this;
+		}
+		
+		/**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $photoFile
+     */
+		public function setPhotoFile(?File $photoFile = null): void
+    {
+				$this->photoFile = $photoFile;
+				
+			if (null !== $photoFile) {
+				// It is required that at least one field changes if you are using doctrine
+        // otherwise the event listeners won't be called and the file is lo
+				$this->updatedAt = new \DateTimeImmutable();
+			}
+		}
+
+		public function getPhotoFile(): ?File
+    {
+        return $this->photoFile;
+		}
+
+    public function setPhotoName(?string $photoName): void
+    {
+				$this->photoName = $photoName;
+		}
+
+		public function getPhotoName(): ?string
+    {
+        return $this->photoName;
+		}
+
+		public function setImageSize(?int $imageSize): void
+    {
+        $this->imageSize = $imageSize;
     }
 
-    public function getPhotoFilename(): ?string
+    public function getImageSize(): ?int
     {
-        return $this->photoFilename;
+        return $this->imageSize;
     }
 
-    public function setPhotoFilename(?string $photoFilename): self
+    public function getUpdatedAt(): ?\DateTimeInterface
     {
-        $this->photoFilename = $photoFilename;
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
 
         return $this;
     }
